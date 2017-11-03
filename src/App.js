6748a395933020e1c04109eee494af782f0baabf
David Skaggs <davidtskaggs@gmail.com>
2017-11-03 11:16:40 -0400

    Add title and clock

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
     );
+
+  }
+  compondendWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+     return (
+       <div>
+        <h1>IMDB Clone </h1>
+        { <h2>{this.state.date.toLocaleTimeString()}.</h2> }
+      </div>
+     );
   }
 }
 
